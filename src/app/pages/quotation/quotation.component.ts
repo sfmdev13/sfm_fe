6978393d42fcea06eec853cd59efabd3566c7611
gifino1 <gifino92@gmail.com>
2025-02-03 19:43:35 +0700

    fix: quoation issue

--- src/app/pages/quotation/quotation.component.ts
+++ src/app/pages/quotation/quotation.component.ts
@@ -267,7 +267,7 @@ export class QuotationComponent implements OnInit {
         }
         this.apiSvc.createQuotationLog(body).subscribe({
           next: () => {
-            this.excelQuotSvc.generateExcel(dataBasic, dataDetail, revision, this.productCategory);
+            this.excelQuotSvc.generateExcel(dataBasic, dataDetail, revision, this.productCategory, type);
           }
         })
 
@@ -280,7 +280,7 @@ export class QuotationComponent implements OnInit {
         }
         this.apiSvc.createQuotationLog(body).subscribe({
           next: () => {
-            this.pdfRABService.generatePdf(dataBasic, dataDetail, revision, this.productCategory);
+            this.excelQuotSvc.generateExcel(dataBasic, dataDetail, revision, this.productCategory, type);
           }
         })
       }
